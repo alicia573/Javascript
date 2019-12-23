c982dd47563c16d49ac1e4ffbffc84c5e889525b
alicia573 <54794745+alicia573@users.noreply.github.com>
2019-12-23 08:37:16 +0100

    commit

---
+++ Bestanden/H02/Opdracht-2.1.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById("titel").innerText ="Dit is de paginatitel";
+document.getElementById("subkop").innerText ="Dit is de subkop";
+document.getElementById("img-1").src="img/img-1.jpg";
+document.getElementById("img-2").src="img/img-2.jpg";
+
