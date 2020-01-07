33dd83198e7f88141d230aac226ebd78ac48f41e
alicia573 <54794745+alicia573@users.noreply.github.com>
2020-01-07 09:17:18 +0100

    commit

---
+++ Bestanden/H04/Opdracht-4.1.js
@@ -0,0 +1,22 @@
+var mijnauto = {
+  kleur: "blauw",
+  merk:"Ford",
+  snelheid:0,
+  gasgeven: function () {
+    this.snelheid += 5
+    console.log(this.snelheid)
+  },
+  toeteren: function () {
+    console.log("toet!");
+
+  }
+}
+console.log(mijnauto.kleur);
+console.log(mijnauto.gasgeven());
+console.log(mijnauto.gasgeven());
+console.log(mijnauto.gasgeven());
+console.log(mijnauto.gasgeven());
+console.log(mijnauto.gasgeven());
+console.log(mijnauto.gasgeven());
+console.log(mijnauto.gasgeven());
+console.log(mijnauto.toeteren());
---
+++ Bestanden/H04/Opdracht-4.2.js
@@ -0,0 +1,21 @@
+var plaatjes = [1,2,3];
+var teller= 0;
+var slideholder = document.getElementById("slideholder");
+slideholder.style.backgroundImage= "url('img/fish1.jpg')";
+
+slideholder.addEventListener("click",function () {
+  getFish();
+  slideholder.style.backgroundImage= "url('img/fish" +getFish() +".jpg')";
+
+});
+function getFish(){
+  if(teller >= 9) {
+    teller = 1;
+  }else{
+    teller++;
+  }
+  console.log(teller);
+
+  return teller;
+}
+document.getAnimations("img/fish1.jpg");
