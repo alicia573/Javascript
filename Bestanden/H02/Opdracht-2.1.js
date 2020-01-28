31a0034032820c330f2765182c297ea7755570c6
alicia573 <54794745+alicia573@users.noreply.github.com>
2020-01-28 11:02:30 +0100

    commit

--- Bestanden/H02/Opdracht-2.1.js
+++ Bestanden/H02/Opdracht-2.1.js
@@ -2,6 +2,6 @@
 
 document.getElementById("titel").innerText ="Dit is de paginatitel";
 document.getElementById("subkop").innerText ="Dit is de subkop";
-document.getElementById("img-1").src="Bestanden/img/img-1.jpg";
-document.getElementById("img-2").src="Bestanden/img/img-2.jpg";
+document.getElementById("img-1").src="img-1.jpg";
+document.getElementById("img-2").src="img-2.jpg";
 
