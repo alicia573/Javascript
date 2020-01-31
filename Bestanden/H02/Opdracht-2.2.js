2005d49c5bd64bba9a2924b5fd03d60d54e6dcef
alicia573 <54794745+alicia573@users.noreply.github.com>
2020-01-31 11:04:10 +0100

    commit

--- Bestanden/H02/Opdracht-2.2.js
+++ Bestanden/H02/Opdracht-2.2.js
@@ -1,9 +1,7 @@
 
-document.getElementById("bl1").style.backgroundColor = "deepskyblue";
-document.getElementById("bl2").style.backgroundColor = "green";
-document.getElementById("bl3").style.backgroundColor = "yellow";
-document.getElementById("bl4").style.backgroundColor = "red";
-document.getElementById("bl2").style.marginTop="20px";
-document.getElementById("bl3").style.marginTop="50px";
+document.getElementById("bl1").style.cssText = "box-sizing: border-box;height: 100px; text-align: center; width: 100px;background-color:aqua; float:left;";
+document.getElementById("bl2").style.cssText ="box-sizing: border-box; margin-top: 15px; text-align: center;height: 100px;width: 100px;background-color:green;float: left;\n";
+document.getElementById("bl3").style.cssText = "text-align: right;box-sizing: border-box;margin-top:45px;height: 100px;width: 100px; background-color:yellow;float: left;";
+document.getElementById("bl4").style.cssText = "box-sizing: border-box; height: 100px;width: 100px;background-color:red;float: left;";
 
-
+document.body.style.cssText= " padding:0px;margin:0px; background-color: aliceblue;";
