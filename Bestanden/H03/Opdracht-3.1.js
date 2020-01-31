0430fb9e5634fee3bac088f7d27f5dd3fa7602cb
alicia573 <54794745+alicia573@users.noreply.github.com>
2020-01-31 09:11:28 +0100

    commit

--- Bestanden/H03/Opdracht-3.1.js
+++ Bestanden/H03/Opdracht-3.1.js
@@ -4,5 +4,5 @@ var random= 0;
 
 for( var i = 0; i<plaatjes.length; i++){
   random = Math.floor(Math.random () * 9) +1;
-  plaatjes[i].src = "../H03/img/fish" + random + ".jpg";
+  plaatjes[i].src = "img/fish" + random + ".jpg";
 }
