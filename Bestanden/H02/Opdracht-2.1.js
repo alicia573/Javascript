c17ce296ef0544706cddf038c0b3e0a8694c6689
alicia573 <54794745+alicia573@users.noreply.github.com>
2020-01-31 11:08:54 +0100

    commit

--- Bestanden/H02/Opdracht-2.1.js
+++ Bestanden/H02/Opdracht-2.1.js
@@ -4,5 +4,5 @@ document.getElementById("titel").innerText ="Dit is de paginatitel";
 document.getElementById("subkop").innerText ="Dit is de subkop";
 document.getElementById("img-1").src="img-1.jpg";
 document.getElementById("img-2").src="img-2.jpg";
-document.getElementById("wrapper").style.width="100px";
-document.getElementById("wrapper").style.margin="auto";
+document.getElementById("wrapper").style.cssText="width:1000px; margin:auto;";
+
