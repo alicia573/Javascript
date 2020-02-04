e4f29d180be4d331f90ca99c5d4b0dd118eb420e
alicia573 <54794745+alicia573@users.noreply.github.com>
2020-02-04 09:55:32 +0100

    commit

--- Bestanden/H04/Opdracht-4.3.js
+++ Bestanden/H04/Opdracht-4.3.js
@@ -1,37 +1,35 @@
-var plaatjes = [1,2,3];
 var teller= 0;
-
 var slidertop = document.getElementById("slider-top");
 slidertop.style.backgroundImage= "url('../top/top1.jpg)";
 
-slidertop.addEventListener("click",function () {
-  getTop();
-  slidertop.style.backgroundImage= "url('top/top" +getTop() +".jpg')";
-});
+  slidertop.addEventListener("click",function () {
+    getSlider();
+    slidertop.style.backgroundImage= "url('img/top" +getSlider() +".jpg')";
+  });
 
 var sliderbetween = document.getElementById("slider-between");
 sliderbetween.style.backgroundImage= "url('../top/top1.jpg)";
 
-sliderbetween.addEventListener("click",function () {
-  getTop();
-  sliderbetween.style.backgroundImage= "url('between/between" +getTop() +".jpg')";
-});
+  sliderbetween.addEventListener("click",function () {
+    getSlider();
+    sliderbetween.style.backgroundImage= "url('img/between" +getSlider() +".jpg')";
+  });
 
 var sliderend= document.getElementById("slider-end");
 sliderend.style.backgroundImage= "url('../top/top1.jpg)";
 
-sliderend.addEventListener("click",function () {
-  getTop();
-  sliderend.style.backgroundImage= "url('end/end" +getTop() +".jpg')";
-});
+  sliderend.addEventListener("click",function () {
+    getSlider();
+    sliderend.style.backgroundImage= "url('img/end" +getSlider() +".jpg')";
+  });
 
 
-function getTop(){
-  if(teller >= 3) {
-    teller = 1;
-  }else{
-    teller++;
+  function getSlider(){
+    if(teller >= 3) {
+      teller = 1;
+    }else{
+      teller++;
+    }
+    console.log(teller);
+    return teller;
   }
-  console.log(teller);
-  return teller;
-}
